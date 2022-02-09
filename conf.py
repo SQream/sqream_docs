b33d773db0003549e9e5be1381bb9920500bf6ab
Yaniv Gerowitz <yanivg@sqreamtech.com>
2022-02-09 09:28:13 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -26,7 +26,7 @@ author = 'Sean Tomarian'
 
 # The full version, including alpha/beta/rc tags
 
-release = '2021.2'
+release = '2021.1'
 
 
 # -- General configuration ---------------------------------------------------
