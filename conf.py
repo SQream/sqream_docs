1719c9fb7f4c7806f99958c82bd0e5c1106e0210
RosinTalia <117294941+TaliaSQream@users.noreply.github.com>
2023-05-11 13:05:54 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -34,7 +34,6 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme',
     'notfound.extension', # 404 handling
     'sphinx_copybutton'
 ]
