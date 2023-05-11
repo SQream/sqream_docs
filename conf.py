3f31edb3f4780173aef00267a055a918378a6040
RosinTalia <117294941+TaliaSQream@users.noreply.github.com>
2023-05-11 14:23:08 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -97,6 +97,7 @@ html_theme_options = {
    , 'dark_logo': 'SQream_logo_dark_mode.png'
    , 'light_logo': 'SQream_logo_bright_mode.png'
    , "sidebar_hide_name": True
+   , "footer_icons":
    
 
 }
