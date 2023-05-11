5f2089831483cdd542d53c76f1adf98d59a5e255
RosinTalia <117294941+TaliaSQream@users.noreply.github.com>
2023-05-11 09:57:54 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@
 
 import sphinx_rtd_theme
 
-# sys.path.append(os.path.abspath('c:\python311\lib\site-packages'))
+# sys.path.append(os.path.abspath('c:/python311/lib/site-packages'))
 
 # -- Project information -----------------------------------------------------
 
