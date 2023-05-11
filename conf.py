70177676360ff6f8519971a097a73b6cba898dd9
RosinTalia <117294941+TaliaSQream@users.noreply.github.com>
2023-05-11 16:32:03 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -57,6 +57,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = "furo"
 
+pygments_style = "sphinx"
+pygments_dark_style = "monokai"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
