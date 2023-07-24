b1797518d6abcb39114260137af891d7f360325d
RosinTalia <117294941+TaliaSQream@users.noreply.github.com>
2023-07-24 16:27:38 +0300

    Update connection_string.java

--- data_ingestion/connection_string.java
+++ data_ingestion/connection_string.java
@@ -1,4 +1,6 @@
-##Postgresql, Oracle, and SQreamDB Connection Strings - mandatory!
+####mandatory!
+
+##Postgresql, Oracle, and SQreamDB Connection Strings
 	
 #postgres
 source.jdbc.connectionstring=jdbc:postgresql://<HostIp:port>/<database_name>?user=<user_name>&password=<password>&ssl=<true/false>
@@ -9,11 +11,21 @@ source.jdbc.connectionstring=jdbc:oracle:thin:@//<HostIp:port>/<database_name>?u
 #sqream
 sqream.jdbc.connectionstring=jdbc:Sqream://<HostIp:port>/<database_name>;cluster=<true/false>;user=<user_name>;password=<password>
 	
+##CDC and Incremental Parameter
 
-##Optional 
+cdc_catalog_table=SMSDW.CDC_TABLES
+cdc_tracking_table=SMSDW.CDC_TRACKING
+cdc_primary_keys_table=SMSDW.CDC_TABLE_PRIMARY_KEYS
+
+
+
+####Optional!
 	
-#Oracle
+#Oracle Catalog
 #catalog.jdbc.connectionstring=jdbc:oracle:thin:@//<HostIp:port>/<database_name>?user=<user_name>&password=<password>
 
-#SQreamDB
+#SQreamDB Catalog
 #catalog.jdbc.connectionstring=jdbc:Sqream://<HostIp:port>/<database_name>;cluster=<true/false>;user=<user_name>;password=<password>
+
+#Summary Parameter
+#load_summary_table=SMSDW.SQLOAD_SUMMARY
