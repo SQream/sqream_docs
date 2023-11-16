21e1f313415fbb95e3497cbf0c91d3551aaf7daa
RosinTalia <117294941+TaliaSQream@users.noreply.github.com>
2023-11-16 08:55:21 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -66,7 +66,7 @@ html_theme = "furo"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-favicon = [
+favicons = [
     "favicon_01.png"
 ]
 
