2f52f81f4dead80c63f0d49081a9f7252d43ce32
RosinTalia <117294941+TaliaSQream@users.noreply.github.com>
2023-11-28 14:54:35 +0200

    SQLoader 7.8.1

--- data_ingestion/connection_string.java
+++ data_ingestion/connection_string.java
@@ -1,6 +1,6 @@
 ####mandatory!
 
-##Postgresql, Oracle, and SQreamDB Connection Strings
+##Postgresql, Oracle, Teradata, SAP HANA, Microsoft SQL Server, and SQreamDB Connection Strings
 	
 #postgres
 source.jdbc.connectionstring=jdbc:postgresql://<HostIp:port>/<database_name>?user=<user_name>&password=<password>&ssl=<true/false>
@@ -8,14 +8,23 @@ source.jdbc.connectionstring=jdbc:postgresql://<HostIp:port>/<database_name>?use
 #oracle
 source.jdbc.connectionstring=jdbc:oracle:thin:@//<HostIp:port>/<database_name>?user=<user_name>&password=<password>&ssl=<true/false>
 	
+#teradata
+source.jdbc.connectionstring=jdbc:teradata://<HostIp>/DATABASE=<database_name>,DBS_PORT=<port>,user=<user_name>,password=<password>
+
+#sap hana
+source.jdbc.connectionstring=jdbc:sap://<HostIp>:<port>/?user=<user_name>&password=<password>
+
+#microsoft sql server
+source.jdbc.connectionstring=jdbc:sqlserver://<HostIp>:<port>;databaseName=<database_name>;user=<user_name>;password=<password>;encrypt=<true/false>;trustServerCertificate=<true/false>	
+	
 #sqream
 sqream.jdbc.connectionstring=jdbc:Sqream://<HostIp:port>/<database_name>;cluster=<true/false>;user=<user_name>;password=<password>
 	
 ##CDC and Incremental Parameter
 
-cdc_catalog_table=SMSDW.CDC_TABLES
-cdc_tracking_table=SMSDW.CDC_TRACKING
-cdc_primary_keys_table=SMSDW.CDC_TABLE_PRIMARY_KEYS
+cdc_catalog_table=public.CDC_TABLES
+cdc_tracking_table=public.CDC_TRACKING
+cdc_primary_keys_table=public.CDC_TABLE_PRIMARY_KEYS
 
 
 
@@ -28,4 +37,4 @@ cdc_primary_keys_table=SMSDW.CDC_TABLE_PRIMARY_KEYS
 #catalog.jdbc.connectionstring=jdbc:Sqream://<HostIp:port>/<database_name>;cluster=<true/false>;user=<user_name>;password=<password>
 
 #Summary Parameter
-#load_summary_table=SMSDW.SQLOAD_SUMMARY
+#load_summary_table=public.SQLOAD_SUMMARY
