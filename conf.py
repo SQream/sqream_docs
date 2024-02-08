e76f52b5e2c081b691e2da1a65ba19c6684fa8c7
RosinTalia <117294941+TaliaSQream@users.noreply.github.com>
2024-02-08 17:02:04 +0200

    css

--- conf.py
+++ conf.py
@@ -64,14 +64,14 @@ html_favicon = "_static/images/faviconSQ.png"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static/images']
+html_static_path = ['_static/images', '_static/css']
 
 favicons = [
     "_static/images/faviconSQ.png"
 ]
 
 html_css_files = [
-    'css/custom.css', # Relative to the _static path
+    'custom.css', # Relative to the _static path
 ]
 
 html_logo = '_static'
