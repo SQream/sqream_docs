d0d8e6a47e6312ae8c66e1f8a577034390de73c8
RosinTalia <117294941+TaliaSQream@users.noreply.github.com>
2024-07-17 10:58:12 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # Define the canonical URL if you are using a custom domain on Read the Docs
 html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "https://readthedocs.com/projects/sqream-sqream-docs/")
 
-# Tell Jinja2 templates the build is running on Read the Docs
-if os.environ.get("READTHEDOCS", "https://readthedocs.com/projects/sqream-sqream-docs/") == "True":
-    html_context["READTHEDOCS"] = True
+
 
 
 
