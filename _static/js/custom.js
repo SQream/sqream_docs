94802d441658053610a56ad34a58bec0c4d4a2d7
RosinTalia <117294941+TaliaSQream@users.noreply.github.com>
2024-07-24 11:05:42 +0300

    Update custom.js

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Select the flyout menu container and hide it
-    var flyoutMenu = document.querySelector('.floating.container.bottom-right');
+    var flyoutMenu = document.querySelector("body > readthedocs-flyout").shadowRoot.querySelector("div");
     if (flyoutMenu) {
         flyoutMenu.style.display = 'none';
     }
 });
+
+
+
