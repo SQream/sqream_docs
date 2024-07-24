1d0ee12289a51964e4d3fe5c50a271ec6a12fa46
RosinTalia <117294941+TaliaSQream@users.noreply.github.com>
2024-07-24 11:08:52 +0300

    Update custom.js

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Select the flyout menu container and hide it
-    var flyoutMenu = document.querySelector("body > readthedocs-flyout").shadowRoot.querySelector("div");
+    var flyoutMenu = document.querySelector("body > readthedocs-flyout");
     if (flyoutMenu) {
         flyoutMenu.style.display = 'none';
     }
