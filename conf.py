020e037ae3bb85f3363754a62d8575c18863aedb
RosinTalia <117294941+TaliaSQream@users.noreply.github.com>
2024-07-25 09:53:41 +0300

    Revert "Flyout menu addons"
    This reverts commit 84eeb1ed7af3fa74faf1c00921506b70a0674d8c.

--- conf.py
+++ conf.py
@@ -74,9 +74,7 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 
-html_js_files = [
-    'custom.js', # Relative to the _static path
-]
+
 
 html_css_files = [
     'css/custom.css', # Relative to the _static path
