a76ee6a42f15e920f37235f698991fd99fef335e
RosinTalia <117294941+TaliaSQream@users.noreply.github.com>
2024-07-25 13:49:37 +0300

    Addons

--- conf.py
+++ conf.py
@@ -23,11 +23,6 @@ if os.environ.get("READTHEDOCS", "") == "True":
 
 import sphinx_rtd_theme
 
-# Overrides RTD Addons Flyout menu to appear on the left hand side 
-def setup(app):
-    app.add_css_file('css/custom.css')
-    app.add_css_file('css/custom_flyout.css')
-    app.add_js_file('js/custom.js')  # Ensure you use the correct path
 
 
 
@@ -84,7 +79,6 @@ html_static_path = ['_static']
 
 html_css_files = [
     'css/custom.css', # Relative to the _static path
-    'css/custom_flyout.css'
 ]
 
 html_logo = '_static/images/SQream_logo_without background-15.png'
