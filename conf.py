7ffe9e2057d1209d6ca23fe54af18bd6d16b69ff
shaharf111 <112806728+shaharf111@users.noreply.github.com>
2025-06-09 14:46:39 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -32,7 +32,7 @@ author = 'SQreamDB Documentation'
 
 
 # The full version, including alpha/beta/rc tags
-release = '4.12'
+release = '4.13'
 
 # -- General configuration ---------------------------------------------------
 
