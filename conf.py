2a4b68bffc43d4a2609c7989cdb972699c6de7af
inbarb <80248742+inbarb@users.noreply.github.com>
2025-07-08 15:39:22 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -26,13 +26,11 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # -- Project information -----------------------------------------------------
 
-project = 'SQreamDB'
+project = 'SQream AI ML'
 copyright = '2025 SQreamDB'
 author = 'SQreamDB Documentation'
 
 
-# The full version, including alpha/beta/rc tags
-release = '4.12'
 
 # -- General configuration ---------------------------------------------------
 
@@ -86,6 +84,9 @@ show_authors = False
 html_show_sphinx = False
 
 
+#Hide the sidebar
+html_sidebars = {}
+
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'autumn'
 
@@ -102,6 +103,12 @@ html_theme_options = {
    , 'collapse_navigation': False
    , 'titles_only': True
    , 'flyout_display': 'attached'
+   , "sidebar_hide_name": True
+   , "footer_icons": 'None'
+   , "light_css_variables": {
+        "font-stack": "Arial, sans-serif",
+        "font-stack--monospace": "Courier, monospace"
+   , "sticky_navigation": True        
 
 }
 
