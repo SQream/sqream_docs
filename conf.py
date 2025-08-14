5f8a09901134dc9531838163a7c5cd8690dd6bc3
inbarb <80248742+inbarb@users.noreply.github.com>
2025-08-14 16:55:57 +0300

    4.14 updates

--- conf.py
+++ conf.py
@@ -32,7 +32,7 @@ author = 'SQreamDB Documentation'
 
 
 # The full version, including alpha/beta/rc tags
-release = '4.13'
+release = '4.14'
 
 # -- General configuration ---------------------------------------------------
 
